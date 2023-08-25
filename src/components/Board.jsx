e9d21c82b4e959a222ea6faeb306196b900e9198
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-25 14:12:49 +0100

    Clean up Board implementation
    The board shouldn't handle card creation, should just place them.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,36 +1,7 @@
-import shuffle from '../modules/shuffle';
-import Pokemon from '../modules/pokemon';
-import { useEffect, useState } from 'react';
-
-const populateBoard = async (setPokemon) => {
-  const pokemon = [];
-  const MAX_POKEMON = 150;
-  const BOARD_SIZE = 12;
-
-  for (let i = 0; i < BOARD_SIZE; i++) {
-    const id = Math.floor(Math.random() * (MAX_POKEMON - 1 + 1) + 1);
-    pokemon.push(await Pokemon(id));
-  }
-
-  setPokemon(pokemon);
-};
-
 const Board = () => {
-  const [pokemon, setPokemon] = useState([{}]);
-
-  useEffect(() => {
-    populateBoard(setPokemon);
-  }, []);
-
-  pokemon.forEach((monster) => {
-    console.log(monster.name);
-  });
-
   return (
     <div className="board">
-      {pokemon.forEach((monster) => (
-        <p>{monster.name}</p>
-      ))}
+      <p>test</p>
     </div>
   );
 };
