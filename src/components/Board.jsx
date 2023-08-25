4246a750a8b99d160ba854005231d3b6f613ab0a
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-25 15:06:02 +0100

    Load random cards on board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,21 @@
 import Card from './Card';
 
+const LoadRandomCard = () => {
+  const MAX = 150;
+  const MIN = 1;
+  const id = Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
+
+  return (
+    <>
+      <Card id={id} />
+    </>
+  );
+};
+
 const Board = () => {
   return (
     <div className="board">
-      <Card id={1} />
+      <LoadRandomCard />
     </div>
   );
 };
